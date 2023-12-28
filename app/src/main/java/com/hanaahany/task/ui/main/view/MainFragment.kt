74f9f7b0cfb048ae2166fa3223977c7669501fd7
Hanaa Hany <hanaahany822@gmail.com>
2023-12-28 03:11:26 +0200

    create view for all repo

---
+++ app/src/main/java/com/hanaahany/task/ui/main/view/MainFragment.kt
@@ -0,0 +1,65 @@
+package com.hanaahany.task.ui.main.view
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hanaahany.task.R
+import com.hanaahany.task.databinding.FragmentMainBinding
+import com.hanaahany.task.model.response.allrepo.AllRepoResponse
+import com.hanaahany.task.remote.ApiState
+import com.hanaahany.task.ui.base.BaseFragment
+import com.hanaahany.task.ui.main.viewmodel.MainViewModel
+import kotlin.math.log
+
+
+class MainFragment : BaseFragment<FragmentMainBinding, MainViewModel>() {
+    private val TAG = "TAG MainFragment"
+    override val layoutIdFragment = R.layout.fragment_main
+    override fun getViewModelClass() = MainViewModel::class.java
+    private lateinit var allRepoAdapter: ReposAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.getAllRepo()
+        observeProductListState()
+
+
+    }
+
+    private fun observeProductListState() {
+        collectLatestFlowOnLifecycle(viewModel.allRepo) { state ->
+            when (state) {
+                is ApiState.Failure -> {
+                    Log.i(TAG, "observeProductListState: failure ${state.msg}")
+                }
+
+                ApiState.Loading -> {
+                    Log.i(TAG, "observeProductListState: loading...")
+                }
+
+                is ApiState.Success -> {
+                    Log.i(TAG, "observeProductListState: success ${state.data}")
+                    if (state.data.isEmpty()) {
+
+                    } else {
+                        //cartAdapter.submitList
+                        Log.i(TAG,""+state.data)
+
+                        allRepoAdapter = ReposAdapter(requireContext()) {
+//                            val action=OrderFragmentDirections.actionOrderFragmentToOrderDetailsFragment(it)
+//                            Navigation.findNavController(requireView()).navigate(action)
+                        }
+
+                        allRepoAdapter.submitList(state.data)
+                        binding.recyclerMainRepo.adapter = allRepoAdapter
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/hanaahany/task/ui/main/view/ReposAdapter.kt
@@ -0,0 +1,71 @@
+package com.hanaahany.task.ui.main.view
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.hanaahany.task.databinding.RepoItemBinding
+import com.hanaahany.task.model.response.allrepo.AllRepoResponse
+import com.hanaahany.task.model.response.allrepo.AllRepoResponseItem
+import com.hanaahany.task.model.ui.allrepo.AllRepoItem
+
+class ReposAdapter (var context: Context, private val onClick:(Long) -> Unit) :
+    ListAdapter<AllRepoItem, ReposAdapter.ReposVH>(RecyclerDiffUtilOrdersItem()) {
+    private lateinit var binding: RepoItemBinding
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReposVH {
+        val inflater: LayoutInflater =
+            parent.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        binding = RepoItemBinding.inflate(inflater, parent, false)
+        Log.i("OrderFragmentAdapter","test1")
+        return ReposVH(binding)
+    }
+
+    override fun onBindViewHolder(holder: ReposVH, position: Int) {
+        val currentItem = getItem(position)
+        holder.onBind(currentItem)
+        Log.i("OrderFragmentAdapter","test1")
+
+    }
+
+    inner class ReposVH(var binding: RepoItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+
+        fun onBind(currentItem: AllRepoItem) {
+
+            binding.apply {
+                tvRepoOwner.text = currentItem.owner?.login
+
+                tvRepoName.text=currentItem.name
+                tvRepoDesc.text=currentItem.description
+                tvStarCount.text=currentItem.owner?.id.toString()
+                Glide.with(context).load(currentItem.owner?.avatarUrl).into(imageRepo)
+
+
+            }
+            itemView.setOnClickListener{
+                //onClick()
+            }
+
+        }
+    }
+}
+
+class RecyclerDiffUtilOrdersItem : DiffUtil.ItemCallback<AllRepoItem>() {
+    override fun areItemsTheSame(
+        oldItem: AllRepoItem, newItem: AllRepoItem
+    ): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(
+        oldItem: AllRepoItem, newItem: AllRepoItem
+    ): Boolean {
+        return oldItem == newItem
+    }
+}
