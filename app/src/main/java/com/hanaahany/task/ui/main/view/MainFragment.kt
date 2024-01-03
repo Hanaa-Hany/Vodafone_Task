975a0d8f4eac9a26184ad9d7567fe23005c16975
Hanaa Hany <hanaahany822@gmail.com>
2024-01-03 08:16:20 +0200

    update viewmodel for search

--- app/src/main/java/com/hanaahany/task/ui/main/view/MainFragment.kt
+++ app/src/main/java/com/hanaahany/task/ui/main/view/MainFragment.kt
@@ -1,21 +1,17 @@
 package com.hanaahany.task.ui.main.view
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.View
-import androidx.core.view.isVisible
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
-import androidx.paging.LoadState
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.hanaahany.task.R
 import com.hanaahany.task.databinding.FragmentMainBinding
+import com.hanaahany.task.model.ui.allrepo.AllRepoItem
 import com.hanaahany.task.remote.ApiState
 import com.hanaahany.task.ui.base.BaseFragment
 import com.hanaahany.task.ui.main.viewmodel.MainViewModel
-import kotlinx.coroutines.launch
 
 
 class MainFragment : BaseFragment<FragmentMainBinding, MainViewModel>() {
@@ -23,46 +19,78 @@ class MainFragment : BaseFragment<FragmentMainBinding, MainViewModel>() {
     override val layoutIdFragment = R.layout.fragment_main
     override fun getViewModelClass() = MainViewModel::class.java
     private lateinit var allRepoAdapter: ReposAdapter
+    private var repoList: List<AllRepoItem> = emptyList()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-//        binding.recyclerMainRepo.apply {
-//            layoutManager = LinearLayoutManager(requireContext())
-//            val decoration = DividerItemDecoration(requireContext(), LinearLayoutManager.VERTICAL)
-//            addItemDecoration(decoration)
-//            adapter = viewModel.dataAdapter.withLoadStateFooter(LoadMoreAdapter())
-//        }
-//
-//        viewModel.repositories
-//        initObserver()
-//
-//    }
-//
-//    private fun initObserver() {
-//
-//        lifecycleScope.launch{
-//            viewModel.repositories.collect {
-//                viewModel.setAdapterData(it)
-//                Log.i(TAG,it.toString())
-//            }
-//        }
-//
-//        lifecycleScope.launchWhenCreated {
-//            viewModel.dataAdapter.loadStateFlow.collect{
-//                val state = it.refresh
-//                binding.progressbar.isVisible = state is LoadState.Loading
-//            }
-//        }
-//
-//    }
-//
-//
-        viewModel.getAllRepo()
         observeIssueListState()
 
 
+
+        binding.etSearch.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+                val searchText = p0.toString().trim()
+                if (searchText.isEmpty()){
+                    observeIssueListState()
+                }else{
+                    viewModel.filterRepo(searchText)
+                }
+
+
+                Log.i(TAG,searchText)
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+        }
+        )
+
+        filterList()
     }
+
+    private fun filterList() {
+        collectLatestFlowOnLifecycle(viewModel.filterRepo) { state ->
+            when (state) {
+                is ApiState.Failure -> {
+
+                }
+
+                ApiState.Loading -> {
+
+                }
+
+                is ApiState.Success -> {
+                    Log.i(TAG, "observeProductListState: success ${state.data}")
+                    if (state.data.isEmpty()) {
+
+                    } else {
+                        //cartAdapter.submitList
+                        Log.i(TAG, "" + state.data)
+
+                        allRepoAdapter = ReposAdapter(requireContext()) { first, second ->
+                            val action =
+                                MainFragmentDirections.actionMainFragmentToRepoDetailsFragment(
+                                    first,
+                                    second
+                                )
+                            Navigation.findNavController(requireView()).navigate(action)
+                        }
+
+                        allRepoAdapter.submitList(state.data)
+                        binding.recyclerMainRepo.adapter = allRepoAdapter
+
+                    }
+                }
+            }
+        }
+    }
+
 
     private fun observeIssueListState() {
         collectLatestFlowOnLifecycle(viewModel.allRepo) { state ->
@@ -77,19 +105,19 @@ class MainFragment : BaseFragment<FragmentMainBinding, MainViewModel>() {
                     Log.i(TAG, "observeProductListState: loading...")
                     binding.lottiLoad.visibility = View.GONE
                     binding.lottiError.visibility = View.GONE
-                    binding.fragmentMainShammer.visibility=View.VISIBLE
+                    binding.fragmentMainShammer.visibility = View.VISIBLE
                     binding.fragmentMainShammer.startShimmerAnimation()
                 }
 
                 is ApiState.Success -> {
-                    binding.fragmentMainShammer.visibility=View.GONE
+                    binding.fragmentMainShammer.visibility = View.GONE
                     binding.lottiLoad.visibility = View.GONE
                     binding.lottiError.visibility = View.GONE
                     Log.i(TAG, "observeProductListState: success ${state.data}")
                     if (state.data.isEmpty()) {
 
                         binding.fragmentMainShammer.stopShimmerAnimation()
-                        binding.lottiEmpty.visibility=View.VISIBLE
+                        binding.lottiEmpty.visibility = View.VISIBLE
                     } else {
                         //cartAdapter.submitList
                         Log.i(TAG, "" + state.data)
@@ -111,4 +139,7 @@ class MainFragment : BaseFragment<FragmentMainBinding, MainViewModel>() {
             }
         }
     }
+
+
+
 }
--- app/src/main/java/com/hanaahany/task/ui/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/hanaahany/task/ui/main/viewmodel/MainViewModel.kt
@@ -11,6 +11,7 @@ import androidx.paging.cachedIn
 import com.hanaahany.task.model.response.allrepo.AllRepoResponse
 import com.hanaahany.task.model.response.allrepo.AllRepoResponseItem
 import com.hanaahany.task.model.ui.allrepo.AllRepoItem
+import com.hanaahany.task.model.ui.search.Item
 import com.hanaahany.task.remote.ApiState
 import com.hanaahany.task.repo.Repo
 import com.hanaahany.task.ui.main.view.DataAdapter
@@ -19,6 +20,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import retrofit2.http.Query
 
 class MainViewModel(private val repo: Repo) : ViewModel() {
     private val TAG = "TAG MainViewModel"
@@ -27,12 +29,26 @@ class MainViewModel(private val repo: Repo) : ViewModel() {
     val allRepo: StateFlow<ApiState<List<AllRepoItem>>>
         get() = _allRepo
 
+    private val _filterRepo = MutableStateFlow<ApiState<List<AllRepoItem>>>(ApiState.Loading)
+    val filterRepo: StateFlow<ApiState<List<AllRepoItem>>>
+        get() = _filterRepo
+
     fun getAllRepo(){
         viewModelScope.launch(Dispatchers.IO) {
             repo.getAllRepo().collectLatest { state ->
                 _allRepo.value = state
             }
         }
+    }
+    fun filterRepo(query: String){
+        viewModelScope.launch(Dispatchers.IO) {
+            repo.searchRepo(query).collectLatest { state->
+                _filterRepo.value = state
+            }
+        }
+    }
+    init{
+        getAllRepo()
     }
 
 //    var dataAdapter: DataAdapter = DataAdapter()
